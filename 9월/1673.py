451668596a2f9e1c18045a0f0b8250cbbdb89c93
leejy96kr <leejy96kr@naver.com>
2022-09-04 04:52:50 +0900

    9월

--- 9월/1673.py
+++ 9월/1673.py
@@ -1,16 +1,14 @@
-try:
-    n, k = map(int, input().split())
 
-    ans = n
-    while True:
-        c = n//k
+n, k = map(int, input().split())
+
+ans = n
+while True:
+    c = n//k
+    ans += c
+    n = c + n%k
+    if c == 0:
         ans += c
-        n = c
-        if c == 0:
-            ans += c
-            print(ans)
-            break
-        else:
-            continue
-except:
-    exit()
+        print(ans)
+        break
+    else:
+        continue
