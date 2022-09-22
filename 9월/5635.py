3de237afd320285228500e27118e5e0be6934c74
leejy96kr <leejy96kr@naver.com>
2022-09-23 00:31:07 +0900

    9월

---
+++ 9월/5635.py
@@ -0,0 +1,8 @@
+lst = []
+for _ in range(int(input())):
+    n,d,m,y = input().rstrip().split()
+    d,m,y = map(int,(d,m,y))
+    lst.append((y,m,d,n))
+lst.sort()
+print(lst[-1][3])
+print(lst[0][3])
